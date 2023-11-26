a54fd163be5b11b44f5ff29cce0608913dacf4c2
tanvirfsd <tahasanpro@gmail.com>
2023-11-26 14:35:13 +0600

    create homepage with react-router-dom

---
+++ src/Layouts/Main.jsx
@@ -0,0 +1,13 @@
+import { Outlet } from 'react-router-dom'
+const Main = () => {
+  return (
+    <div>
+      
+      <div className=''>
+        <Outlet />
+      </div>
+    </div>
+  )
+}
+
+export default Main
